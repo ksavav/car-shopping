288bc56066eec8ca1c4276acd8bdc5b08b832a32
Ksawery Armatynski <a.xawery@gmail.com>
2025-05-21 21:53:31 +0200

    added todos

--- client/src/app/models/userParams.ts
+++ client/src/app/models/userParams.ts
@@ -1,6 +1,12 @@
 export class UserParams {
     pageNumber = 1
     pageSize = 5
-
+    
+    /* TODO
+     * Add params like:
+     * - category
+     * - producer
+     * - price range
+     */
     constructor() { }
 }
