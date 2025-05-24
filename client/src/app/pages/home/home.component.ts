cd7069bd2ce9303fdae8c9c5b04833d579a9398b
Ksawery Armatynski <a.xawery@gmail.com>
2025-05-25 00:43:42 +0200

    carousel goes brrrrr

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { PageService } from '../../services/page.service';
 import { Page } from '../../models/page';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
+import { ProductCardComponent } from "./product-card/product-card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProductCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
   pageSections: { [key: string]: string } = {};
+  originalItems: Product[] = [];
+  items: any[] = [];
+  currentSlide = 3;
+  visibleCount = 3;
+  autoplayInterval: any;
+  transitionEnabled = true;
 
-  constructor(private pageService: PageService) {}
+  constructor(@Inject(PLATFORM_ID) private platformId: any, private pageService: PageService, private productService: ProductService) {}
 
   ngOnInit(): void {
     this.pageService.getPage('home').subscribe({
@@ -25,7 +34,54 @@ export class HomeComponent implements OnInit {
         console.error('Error fetching pages:', err);
       }
     });
+
+    this.productService.getProductsList(["07254QAP", "07502QAP", "07119QAP", "07083QAP", "06551QAP", "06421QAP", "06305QAP"]).subscribe({
+      next: (data: Product[]) => {
+        this.originalItems = data
+        const prepend = this.originalItems.slice(-this.visibleCount);
+        const append = this.originalItems.slice(0, this.visibleCount);
+        this.items = [...prepend, ...this.originalItems, ...append];
+      }
+    })
+
+    if (isPlatformBrowser(this.platformId)) {
+      this.startAutoplay();
+    }
   }
 
-  
+  ngOnDestroy() {
+    this.stopAutoplay();
+  }
+
+  startAutoplay() {
+    this.autoplayInterval = setInterval(() => this.next(), 5000); // every 3s
+  }
+
+  stopAutoplay() {
+    clearInterval(this.autoplayInterval);
+  }
+
+  next() {
+    this.currentSlide++;
+    this.transitionEnabled = true;
+
+    if (this.currentSlide === this.items.length - this.visibleCount) {
+      setTimeout(() => {
+        this.transitionEnabled = false;
+        this.currentSlide = this.visibleCount;
+      }, 300); // same as CSS transition time
+    }
+  }
+
+  prev() {
+    this.currentSlide--;
+    this.transitionEnabled = true;
+
+    if (this.currentSlide === 0) {
+      setTimeout(() => {
+        this.transitionEnabled = false;
+        this.currentSlide = this.items.length - this.visibleCount * 2;
+      }, 300);
+    }
+  }
 }
